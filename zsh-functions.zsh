ad8472b5025a5ffa57479de1cd99c45ab07f45f7
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2020-02-07 15:30:16 +0900

    simplify git command

--- zsh-functions.zsh
+++ zsh-functions.zsh
@@ -159,7 +159,7 @@ function return-to-default-branch-and-delete-merged-topic-branch() {
   if [[ $(git status 2> /dev/null) ]]; then
     # Get default and topic branch.
     local default_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
-    local topic_branch=$(git branch | grep -E '^\*\s+' | awk '{print $2}')
+    local topic_branch=$(git rev-parse --abbrev-ref HEAD)
     if [[ -z $default_branch ]] || [[ -z $topic_branch ]]; then
       tput setaf 1 && \
         printf "Cannot get default or topic branch!\n"
